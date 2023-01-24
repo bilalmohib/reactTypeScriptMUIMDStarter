fdf82afbc50398712845090cb8b59ec44cd427bc
Muhammad-Bilal-7896 <2019cs682@student.uet.edu.pk>
2023-01-25 01:07:31 +0500

    Init

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # React TypeScript based MUI & MD Starter
 
 ## Tech Stacks Used
-- React version = 18.2.0,
-- Material UI = 5.11.5,
-- Typescript = 4.9.4,
-- Md bootstrap(mdb) = 6.1.0
+<ul>
+                            <li>
+                                <a href="https://github.com/facebook/react/blob/main/CHANGELOG.md#1820-june-14-2022">React version = 18.2.0</a>
+                            </li>
+                            <li>
+                                <a href="https://www.npmjs.com/package/react-router-dom/v/6.6.1">React Router & React Router DOM = 6.6.1</a>
+                            </li>
+                            <li>
+                                <a href="https://mui.com/material-ui/getting-started/overview/">Material UI = 5.11.5</a>
+                            </li>
+                            <li>
+                                <a href="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html">Typescript = 4.9.4</a>
+                            </li>
+                            <li>
+                                <a href="https://mdbootstrap.com/docs/standard/">Md bootstrap(mdb) = 6.1.0</a>
+                            </li>
+                            <li>
+                                <a href="https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/">Modular CSS</a>
+                            </li>
+                        </ul>
 
 ## Pre-Requisite
 You must Node JS installed on your system.
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -45,10 +45,24 @@ const Home = () => {
 
                         <h4 className="text-info">Tech Stacks Used</h4>
                         <ul>
-                            <li>React version = 18.2.0</li>
-                            <li> Material UI = 5.11.5</li>
-                            <li> Typescript = 4.9.4 </li>
-                            <li> Md bootstrap(mdb) = 6.1.0 </li>
+                            <li>
+                                <a href="https://github.com/facebook/react/blob/main/CHANGELOG.md#1820-june-14-2022">React version = 18.2.0</a>
+                            </li>
+                            <li>
+                                <a href="https://www.npmjs.com/package/react-router-dom/v/6.6.1">React Router & React Router DOM = 6.6.1</a>
+                            </li>
+                            <li>
+                                <a href="https://mui.com/material-ui/getting-started/overview/">Material UI = 5.11.5</a>
+                            </li>
+                            <li>
+                                <a href="https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html">Typescript = 4.9.4</a>
+                            </li>
+                            <li>
+                                <a href="https://mdbootstrap.com/docs/standard/">Md bootstrap(mdb) = 6.1.0</a>
+                            </li>
+                            <li>
+                                <a href="https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/">Modular CSS</a>
+                            </li>
                         </ul>
                         <br />
                         <Button variant="contained">Press Me</Button>
